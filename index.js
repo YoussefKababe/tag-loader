1a44f10cd114b0e44fa4cc259eaad10332018c78
Youssef Kababe <hello@youssefkababe.com>
2016-06-23 04:23:06 +0000

    Flag loader as cacheable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var loaderUtils = require('loader-utils');
 var riot = require('riot');
 
 module.exports = function(source) {
+  if (this.cacheable) this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
   return 'var riot = require("riot");\n\n' + riot.compile(source, query);
 };
